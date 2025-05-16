0d22d03e774f65715c5f47a4bea303143f2ea147
jerinpaul1 <jerinpaul0208@gmail.com>
2025-05-16 15:02:15 +0100

    Update getGrades.js

--- netlify/functions/getGrades.js
+++ netlify/functions/getGrades.js
@@ -1,23 +1,18 @@
 const { createClient } = require('@supabase/supabase-js');
 
-exports.handler = async function () {
-  const supabase = createClient(process.env.tgnhbmqgdupnzkbofotf.supabase.co , process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRnbmhibXFnZHVwbnprYm9mb3RmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc0MDEyNTYsImV4cCI6MjA2Mjk3NzI1Nn0.gNk-pqah8xdmYjkY0qq217xoezqSVjVWsnasiXRmd1o);
+const supabase = createClient(
+  'https://tgnhbmqgdupnzkbofotf.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRnbmhibXFnZHVwbnprYm9mb3RmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc0MDEyNTYsImV4cCI6MjA2Mjk3NzI1Nn0.gNk-pqah8xdmYjkY0qq217xoezqSVjVWsnasiXRmd1o'
+);
 
+exports.handler = async function () {
   const { data, error } = await supabase
     .from('grades')
-    .select('data')
-    .order('id', { ascending: false })
-    .limit(1);
-
-  if (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: error.message }),
-    };
-  }
+    .select('*')
+    .single();
 
   return {
     statusCode: 200,
-    body: JSON.stringify(data[0]?.data || {}),
+    body: JSON.stringify(data?.data || [])
   };
 };
